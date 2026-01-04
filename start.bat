3d2c232aee27d61640f07b1a01ad863efd5873a3
Caear <muhammadcaesarrifqi@gmail.com>
2026-01-05 02:39:12 +0700

    Fix: Update Tailwind config for v4 compatibility and enhance start script

--- start.bat
+++ start.bat
@@ -4,6 +4,7 @@ echo.
 
 start "Laravel Server" php artisan serve
 start "Queue Worker" php artisan queue:work
+start "Vite Dev" npm run dev
 
 echo Services started!
 echo Server: http://localhost:8000
